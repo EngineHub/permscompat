80707d55ab14475c3c5911bf7517512ed9f08f68
zml <zml@aoeu.xyz>
2015-01-10 16:08:23 -0800

    1.8 - Update PermissionsEx for UUID methods

--- src/main/java/ru/tehkode/permissions/PermissionManager.java
+++ src/main/java/ru/tehkode/permissions/PermissionManager.java
@@ -2,9 +2,11 @@ package ru.tehkode.permissions;
 
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 public class PermissionManager {
-    
-    
+
+
     public boolean has(Player player, String permission) {
         throw new UnsupportedOperationException("Not implemented.");
     }
@@ -13,11 +15,19 @@ public class PermissionManager {
         throw new UnsupportedOperationException("Not implemented.");
     }
 
-
-    public boolean has(String playerName, String permission, String world) {
+    public boolean has(UUID player, String permission, String world) {
         throw new UnsupportedOperationException("Not implemented.");
     }
 
+
+    public PermissionUser getUser(UUID uid) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    // Legacy player name methods
+    public boolean has(String playerName, String permission, String world) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
 
     public PermissionUser getUser(String username) {
         throw new UnsupportedOperationException("Not implemented.");
--- src/main/java/ru/tehkode/permissions/PermissionUser.java
+++ src/main/java/ru/tehkode/permissions/PermissionUser.java
@@ -1,11 +1,17 @@
 package ru.tehkode.permissions;
 
+import java.util.List;
+
 public class PermissionUser {
-    
+
     public boolean inGroup(String group) {
         throw new UnsupportedOperationException("Not implemented.");
     }
-    
+
+    public List<String> getParentIdentifiers() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
     public String[] getGroupsNames() {
         throw new UnsupportedOperationException("Not implemented.");
     }
